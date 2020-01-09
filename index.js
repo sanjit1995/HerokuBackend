70d8e4d7187a4f0997b4a512370a9258d66f3cc4
shibasis <shibasis0801@gmail.com>
2020-01-10 01:34:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const app = express();  
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send("Sanjit Patnaik Portfolio");
+});
+
+app.get('/api', (req, res) => {
+    res.json({
+        name: "Sanjit Patnaik"
+    });
+});
+
+app.listen(PORT);
